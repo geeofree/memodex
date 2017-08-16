7b906feee75554a12ea456d3f359589da44709bc
Geoffrey <geo.galagaran@gmail.com>
2017-08-16 21:24:15 +0800

    Register API resource route component; Add config file reader

--- memodex/webapp.py
+++ memodex/webapp.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config.from_pyfile('config.cfg')
+
 from memodex.views import home, admin
-from memodex.api import resources
+from memodex import resources
 
 app.register_blueprint(home.component)
 app.register_blueprint(admin.component)
-app.register_blueprint(resources.routes)
+app.register_blueprint(resources.component)
