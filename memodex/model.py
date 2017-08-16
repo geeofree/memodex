eab50c7bea83d49f8dc9a2616f106e3d61efc8fb
Geoffrey <geo.galagaran@gmail.com>
2017-08-16 21:25:13 +0800

    Create User table model

---
+++ memodex/model.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from memodex.webapp import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return "<User username: %s, public_id: %s>" % (self.username, self.public_id)
