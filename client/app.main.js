61e3414b43450ab1ea4b509bea18d0402f92a804
Geoffrey <geo.galagaran@gmail.com>
2017-08-27 09:14:17 +0800

    Add login component route

--- client/app.main.js
+++ client/app.main.js
@@ -6,12 +6,14 @@ import Router from './components/Router/app.router'
 
 import Home from './views/home/home.view'
 import Dashboard from './views/dashboard/dashboard.view'
+import Login from './views/login/login.view'
 
 export default () => (
   <Router>
     <Switch>
       <Home path="/" exact={true} />
       <Dashboard path="/dashboard" protected />
+      <Login path="/login" />
     </Switch>
   </Router>
 )
