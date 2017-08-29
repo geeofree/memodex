f89e0515103ec3a823b689d1d5ffa4c25bbae0d9
Geoffrey <geo.galagaran@gmail.com>
2017-08-29 15:34:06 +0800

    Add comments to separate and describe the script

--- server/webapp.py
+++ server/webapp.py
@@ -1,17 +1,22 @@
+# Main Application instance
 from utility.flask_extension import FlaskApp
 app = FlaskApp(__name__)
 app.config.from_pyfile('config.cfg')
 
+# Database ORM instance
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
+# CORS Utility instance
 from flask_cors import CORS
-endpoint = r'%s/*' % app.config.get('API_ENDPOINT')
+endpoint = r'%s/*' % app.config['API_ENDPOINT']
 cors = CORS(app, resources=endpoint)
 
+# AuthToken instance; Must be after DB ORM instantiation to work!
 from server.auth import AuthToken
 auth_token = AuthToken(app.config['SECRET_KEY'])
 
+# Views and Resources registration
 from server.views.app import view as application
 from server.resources import token
 from server.resources import users
